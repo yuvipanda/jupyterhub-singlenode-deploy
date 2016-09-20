b5f826d3b058216cd5db7e152c07a754c4b0a717
YuviPanda <yuvipanda@gmail.com>
2016-09-19 23:56:16 -0700

    Add simple apply script

---
+++ apply.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script that installs puppet if needed & provisions
+# current machine. Expects to run as root.
+
+# If we aren't root, barf
+if [ $(/usr/bin/whoami) != 'root' ]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+# If puppet isn't found, attempt to install it
+if ! /usr/bin/which puppet > /dev/null; then
+    apt-get install --yes puppet
+fi
+
+DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+puppet apply --modulepath="${DIRNAME}/modules" "${DIRNAME}/manifests/default.pp"
