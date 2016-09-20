052d23fb7d13ded519f186647c4a4f0d33996cb0
YuviPanda <yuvipanda@gmail.com>
2016-09-20 00:33:48 -0700

    Fix issues with the nchp_config

--- modules/jupyterhub/files/nchp_config.py
+++ modules/jupyterhub/files/nchp_config.py
@@ -1,5 +1,5 @@
 c.NCHPApp.public_ip = '0.0.0.0'
 c.NCHPApp.public_port = 8000
-c.NCHPApp.api_ip = 127.0.0.1
+c.NCHPApp.api_ip = '127.0.0.1'
 c.NCHPApp.api_port = 8001
-c.NCHPApp.default_target = 'http://127.0.0.1:8080'
+c.NCHPApp.default_target = 'http://127.0.0.1:8081'
