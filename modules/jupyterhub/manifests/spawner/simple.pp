90f1419c217662ba1f6d2e9b31362244bc1c2e38
YuviPanda <yuvipanda@gmail.com>
2016-09-20 19:14:45 -0700

    Add jupyter notebook to jhub venv with simplespawner
    Since it'll require them to be in the same venv to work

--- modules/jupyterhub/manifests/spawner/simple.pp
+++ modules/jupyterhub/manifests/spawner/simple.pp
@@ -17,13 +17,28 @@ define jupyterhub::spawner::simple(
     $base_dir = $::jupyterhub::base::dir
 
     python::pip_package { 'jupyterhub-simplespawner':
-        ensure => present,
+        ensure    => present,
         venv_path => "${base_dir}/${hubname}"
     }
 
     jupyterhub::config { 'simplespawner':
         hubname => $hubname,
         content => "c.JupyterHub.spawner_class = 'simplespawner.SimpleLocalProcessSpawner'",
+    }
+
+    # Setup singleuser stuff in the jupyterhub venv
+
+    python::pip_package { [
+        'jupyter',
+        'notebook',
+    ]:
+        ensure => present,
+        venv_path => "${base_dir}/${hubname}"
+    }
+
+    jupyterhub::config { 'simplespawner-from-venv':
+        hubname => $hubname,
+        content => "c.SimpleLocalProcessSpawner.cmd = ['${base_dir}/${hubname}/bin/jupyterhub-singleuser']"
     }
 
 }
