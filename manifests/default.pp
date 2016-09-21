81a559d086b38b4f1e160e79475d9be498540226
YuviPanda <yuvipanda@gmail.com>
2016-09-20 23:10:32 -0700

    Add localaccounts support to provision unix users
    Also add a sample common.yaml file

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,4 +2,6 @@ node default {
     jupyterhub::hub { 'test': }
     jupyterhub::authenticator::dummy { 'test': }
     jupyterhub::spawner::simple { 'test': }
+
+    include localaccounts
 }
---
+++ modules/localaccounts/manifests/init.pp
@@ -0,0 +1,43 @@
+# = Class: localaccounts
+#
+# Sets up local unix users belonging to a group.
+#
+# This should be used in conjugation with jupyterhub spawners that
+# require users exist as unix users on the system to function properly.
+#
+# It also provides roots with passwordless sudo.
+#
+# FIXME: Find a way to get rid of accounts when they are no longer in use.
+# == Parameters
+#
+# [*group*]
+#  Group to add all users to. Useful when there are users managed via
+#  other means on the system.
+class localaccounts(
+    $group = 'jovian'
+) {
+
+    $regular_users = hiera('accounts::users', [])
+    $root_users = hiera('accounts::roots', [])
+
+    file { '/etc/sudoers.d/localaccounts_sudo':
+        ensure  => present,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => '%sudo ALL=(ALL) NOPASSWD:ALL',
+    }
+
+    group { $group:
+        ensure => present,
+    }
+
+    user { $regular_users:
+        groups => $group
+    }
+
+    user { $root_users:
+        groups => [$group, 'sudo'],
+    }
+ 
+}
