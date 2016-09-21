c9b30ef050e34e9cbb09fd3d639b39bc5f3ec07f
YuviPanda <yuvipanda@gmail.com>
2016-09-20 19:08:52 -0700

    Add simple spawner

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,4 +1,5 @@
 node default {
     jupyterhub::hub { 'test': }
     jupyterhub::authenticator::dummy { 'test': }
+    jupyterhub::spawner::simple { 'test': }
 }
---
+++ modules/jupyterhub/manifests/spawner/simple.pp
@@ -0,0 +1,29 @@
+# = define: jupyterhub::spawner::simple
+#
+# Sets up a simple process spawner (no user isolation) for the jupyterhub
+#
+# == Parameters
+#
+# [*hubname*]
+#  The name of the jupyterhub::hub for which this spawner
+#  is to be set up. Required.
+#
+define jupyterhub::spawner::simple(
+    $hubname = $name,
+) {
+
+    include jupyterhub::base
+
+    $base_dir = $::jupyterhub::base::dir
+
+    python::pip_package { 'jupyterhub-simplespawner':
+        ensure => present,
+        venv_path => "${base_dir}/${hubname}"
+    }
+
+    jupyterhub::config { 'simplespawner':
+        hubname => $hubname,
+        content => "c.JupyterHub.spawner_class = 'simplespawner.SimpleLocalProcessSpawner'",
+    }
+
+}
