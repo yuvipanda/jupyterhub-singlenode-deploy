c22a8e3df9909dc22583c5cf1eca886b9f4ee223
YuviPanda <yuvipanda@gmail.com>
2016-10-11 16:43:06 -0700

    python: Dynamically determine python3 version based on distro

--- modules/python/manifests/base.pp
+++ modules/python/manifests/base.pp
@@ -1,14 +1,14 @@
 # = Class: python::base
 #
 # Sets up a given version of python+virtualenv via the distro's package manager.
-#
-# == Parameters
-# [*version*]
-#  Version of python to install.
-#  FIXME: autodetect based on distro
-class python::base(
-    $version='3.5'
-) {
+class python::base {
+    if $::os['name'] == 'Debian' and $::os['release']['major'] == '8' {
+        # Debian Jessie! Python 3.4
+        $version = '3.4'
+    } else {
+        $version = '3.5'
+    }
+
     # FIXME: Make this work for centos / fedora
     package { "python${version}":
         ensure => present,
